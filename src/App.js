53f6333e6fc20013c5e941a5fd5588ba8ec8acb1
Gryshyn Vitaliy <Vitalygrishin27@gmail.com>
2022-12-03 14:47:15 +0200

    Local Git build

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import Navbar from "./components/Navbar";
 import Subject from "./components/Subject";
 import Clazz from "./components/Clazz";
 import User from "./components/User";
+export const rootUrl = "microfrontend-grade-book";
 
 const App = () => {
+
     return (
         <div className="App">
             <ToastContainer position="bottom-left"/>
             <Navbar/>
             <Routes>
-                <Route path="/" element={<Login/>}/>
-                <Route path="/classes" element={<Clazz/>}/>
-                <Route path="/subjects" element={<Subject/>}/>
-                <Route path="/users" element={<User/>}/>
+                <Route path={rootUrl} element={<Login/>}/>
+                <Route path={rootUrl+"/classes"} element={<Clazz/>}/>
+                <Route path={rootUrl+"/subjects"} element={<Subject/>}/>
+                <Route path={rootUrl+"/users"} element={<User/>}/>
             </Routes>
         </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import {useTranslation} from "react-i18next";
 import {useDispatch, useSelector} from "react-redux";
 import {loginOut} from "../redux/reducers/login/login.thunks";
 import {AccessLevelFilter} from "../types/types";
+import {rootUrl} from "../App";
 
 const Navbar = () => {
     const navigate = useNavigate();
@@ -17,13 +18,13 @@ const Navbar = () => {
     const handleLogOut = () => {
         dispatch(loginOut());
         document.cookie = "grade_book_token=" + escape("") + "; expires=Thu, 01 Jan 1970 00:00:01 GMT"
-        navigate("/")
+        navigate({rootUrl}+"/")
     }
 
     return (
         <div className={"col-md-12 bg-bg-light py-2"}>
             <nav className="navbar navbar-light bg-opacity-100">
-                <Link to="/" className="navbar-brand mx-5">{t("Main")}</Link>
+                <Link to={rootUrl+"/"} className="navbar-brand mx-5">{t("Main")}</Link>
 
                 <div className="mx-3" style={{"textAlign": "right", "display": "inline-block"}}>
                     {/*{isLoginIn &&  <Link to={"/members"} className={"btn btn-success text-light"}>{t("Users")}</Link>}&nbsp;*/}

b93129b1308249d5d8738e49e90428bbaca0cea6
Gryshyn Vitaliy <Vitalygrishin27@gmail.com>
2022-12-03 16:07:24 +0200

    Local Git build

--- src/i18n.js
+++ src/i18n.js
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import {initReactI18next} from "react-i18next";
+import * as path from "path";
 
 i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
     react: {
@@ -9,6 +10,9 @@ i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
     },
     fallbackLng: 'ua',
     debug: false,
+    backend: {
+      loadPath: path.resolve(__dirname,'../locales/{{lng}}/{{ns}}.json')
+    },
     detection: {
         order: ['querystring', 'cookie'],
         cache: ['cookie']
